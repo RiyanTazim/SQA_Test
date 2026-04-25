23f3ecc0ccf06dceab8f5b56b640e625cdaee301
RiyanTazim <tazimt8@gmail.com>
2026-04-25 14:50:37 +0600

    utilities, faker ans user data json

--- playwright/Utils/utilities.js
+++ playwright/Utils/utilities.js
@@ -1,12 +1,21 @@
+import {faker} from "@faker-js/faker"
+
 export class Utilities{
     getRandomNum(min, max){
         const minCeiled = Math.ceil(min);
         const maxFloored = Math.floor(max);
         return Math.floor(Math.random() * (maxFloored - minCeiled + 1)) + minCeiled;
     }
-    randomEmail() {
-        const getRandomNum = this.getRandomNum(5000, 9999999);
-        const randomEmail = `user_${getRandomNum}@mailinator.com`;
-        return randomEmail;
+    userData() {
+        // const getRandomNum = this.getRandomNum(5000, 9999999);
+        // const randomEmail = faker.internet.email();
+        // return randomEmail;
+
+        const user = {
+            firstName: faker.person.firstName(),
+            lastName: faker.person.lastName(),
+            fullname: faker.person.fullName(),
+        };
+        return user;
     }
 }
--- playwright/tests/profile.spec.js
+++ playwright/tests/profile.spec.js
@@ -1,17 +1,30 @@
 import {expect, test} from "@playwright/test";
 import { Profile } from "../pageObjects/profile";
+import { Utilities } from "../Utils/utilities";
+const utilities = new Utilities();
 let user = "";
 
+const fullname = utilities.userData().fullname;
+const userEmail = `${utilities.userData().fullName}@yopmail.com`;
+
 // const profile = new Profile();
-test.describe('Demo Evershop Site Authentication Tests', () => {
-    test.skip('Register with valid information', async ({ page }) => {
+test.describe.serial('Demo Evershop Site Authentication Tests', () => {
+
+    let profile;
+    test.beforeEach(async ({ page }) => {
+        profile = new Profile(page);
+        await page.goto("https://demo.evershop.io/")
+    })
+
+    test('Register with valid information', async ({ page }) => {
         const profile = new Profile(page);
         await page.goto('/');
         await profile.clickOnProfileButton();
         await profile.clickOnCreateAccountLink();
         await page.waitForLoadState('networkidle');
-        await profile.fillFullName("Tazim Tazvi");
-        await profile.fillEmail("tazim@me.com");
+        await profile.fillFullName(fullname);
+        // user = Utilities.prototype.randomEmail();
+        await profile.fillEmail(userEmail);
         await profile.fillPassword("tazim@123");
         // await profile.clickOnSignUpButton();
         await profile.clickOnButton("Sign Up");
@@ -22,7 +35,7 @@ test.describe('Demo Evershop Site Authentication Tests', () => {
         await page.goto('/');
         await profile.clickOnProfileButton();
         await page.waitForLoadState('networkidle');
-        await profile.fillEmail("tazim@me.com");
+        await profile.fillEmail(userEmail);
         await profile.fillPassword("tazim@123");
         // await profile.clickOnSignInButton();
         await profile.clickOnButton("Sign In");
